8828a33c968994af453a66bef2e30e9ee7903a74
Victor Santos Silva <victor.s.silva@unesp.br>
2021-05-27 16:51:28 -0300

    test to model profile has been implemented

---
+++ register/tests/test_model_profile.py
@@ -0,0 +1,34 @@
+import pytest
+from pytest import raises
+from django.urls import reverse
+from model_mommy import mommy
+from register.models import Profile
+
+@pytest.mark.django_db
+def test_publish_profile():
+    """Must register objects in model Profile"""
+    assert Profile.objects.count() == 0
+    mommy.make(Profile)
+    assert Profile.objects.count() == 1
+    mommy.make(Profile)
+    assert Profile.objects.count() == 2
+
+@pytest.mark.django_db
+def test_fields_profile():
+    """Must return true for any field"""
+    assert Profile.user
+    assert Profile.home_planet
+    assert Profile.height
+    assert Profile.mass
+
+@pytest.mark.django_db
+def test_invalid_fields_profile():
+    """Must return attributeerror for any field"""
+    with raises(AttributeError):
+        Profile.usuario
+    with raises(AttributeError):
+        Profile.planeta_natal
+    with raises(AttributeError):
+        Profile.altura
+    with raises(AttributeError):
+        Profile.massa
