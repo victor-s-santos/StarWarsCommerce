4d29822693f353361489e1bbd0a5835abd3c4609
Victor Santos Silva <victor.s.silva@unesp.br>
2021-05-29 23:21:02 -0300

    fix migrations

--- star_wars_ecommerce/settings.py
+++ star_wars_ecommerce/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'star_wars_ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'sb.sqlite3')
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
