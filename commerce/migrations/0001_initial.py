fe7b30446af444f8ef18b0bb4161c414bed4d4ec
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-02 16:00:31 -0300

    persist migration

--- commerce/migrations/0001_initial.py
+++ commerce/migrations/0001_initial.py
@@ -55,3 +55,4 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
+#try again
