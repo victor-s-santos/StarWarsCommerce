3b4bb85675332c4b72be0cc305bbad5e412c6cc1
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-03 15:33:44 -0300

    refactoring automation scripts

--- register_products.py
+++ register_products.py
@@ -2,39 +2,28 @@ import os
 from commerce.models import Product
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-p = Product(product_name='Millenium​ ​Falcon', unit_price=550000.0, multiple=1,
-            image=os.path.join(BASE_DIR, 'product_images/millenium_falcon.png'))
-p.save()
-print('Millenium Falcon has been registered!')
+list_products = [
 
-p = Product(product_name='X-Wing', unit_price=60000.0, multiple=2,
-            image=os.path.join(BASE_DIR, 'product_images/xwing.jpeg'))
-p.save()
-print('X-Wing has been registered!')
+    {'product_name': 'Millenium Falcon', 'unit_price': 550000.0, 'multiple': 1, 
+        'image': os.path.join(BASE_DIR, 'product_images/millenium_falcon.png')},
+    {'product_name': 'X-Wing', 'unit_price': 60000.0, 'multiple': 2, 
+        'image': os.path.join(BASE_DIR, 'product_images/millenium_falcon.png')},
+    {'product_name': 'Super​ ​Star​ ​Destroyer', 'unit_price': 4570000.0, 'multiple': 1, 
+        'image': os.path.join(BASE_DIR, 'product_images/super_star_destroyer.png')},
+    {'product_name': 'TIE Fighter', 'unit_price': 75000.0, 'multiple': 2, 
+        'image': os.path.join(BASE_DIR, 'product_images/tie_fighter.jpeg')},
+    {'product_name': 'Lightsaber', 'unit_price': 6000.0, 'multiple': 5, 
+        'image': os.path.join(BASE_DIR, 'product_images/lightsaber.png')},
+    {'product_name': 'DLT-19 Heavy Blaster Rifle', 'unit_price': 5800.0, 'multiple': 1, 
+        'image': os.path.join(BASE_DIR, 'product_images/DLT-19.png')},
+    {'product_name': 'DL-44 Heavy Blaster Pistol', 'unit_price': 1500.0, 'multiple': 10, 
+        'image': os.path.join(BASE_DIR, 'product_images/DL-44.jpg')}
+]
 
-p = Product(product_name='Super​ ​Star​ ​Destroyer', unit_price=4570000.0, multiple=1,
-            image=os.path.join(BASE_DIR, 'product_images/millenium_falcon.png'))
-p.save()
-print('Super​ ​Star​ ​Destroyer has been registered!')
-
-p = Product(product_name='TIE Fighter', unit_price=75000.0, multiple=2,
-            image=os.path.join(BASE_DIR, 'product_images/tie_fighter.jpeg'))
-p.save()
-print('TIE Fighter has been registered!')
-
-p = Product(product_name='Lightsaber', unit_price=6000.0, multiple=5,
-            image=os.path.join(BASE_DIR, 'product_images/lightsaber.png'))
-p.save()
-print('Lightsaber has been registered!')
-
-p = Product(product_name='DLT-19 Heavy Blaster Rifle', unit_price=5800.0, multiple=1,
-            image=os.path.join(BASE_DIR, 'product_images/DLT-19.png'))
-p.save()
-print('DLT-19 Heavy Blaster Rifle has been registered!')
-
-p = Product(product_name='DL-44 Heavy Blaster Pistol', unit_price=1500.0, multiple=10,
-            image=os.path.join(BASE_DIR, 'product_images/DLT-19.png'))
-p.save()
-print('DL-44 Heavy Blaster Pistol has been registered!')
+for i in list_products:
+    p = Product(product_name=i['product_name'], unit_price=i['unit_price'],
+        multiple=i['multiple'], image=i['image']) 
+    p.save()
+    print(f'{i["product_name"]} has been registered!')
 
 #as imagens não serão hospedadas no cloudinary, infelizmente não foi possível encontrar uma maneira de fazê-lo
--- register_users.py
+++ register_users.py
@@ -1,21 +1,23 @@
 from django.contrib.auth.models import User
 
-u = User(username='darthvader', first_name='Darth', last_name='Vader', email='darth_vader@email.com', is_staff=False)
-u.save()
-print('Darth Vader has been registered!')
+list_names = [
+    {'username': 'darthvader', 'first_name': 'Darth', 'last_name': 'Vader',
+     'email': 'darth_vader@email.com', 'is_staff': False},
 
-u = User(username='obiwan', first_name='Obi-Wan', last_name='Kenobi', email='obi_wan@email.com', is_staff=False)
-u.save()
-print('Obi-Wan Kenobi has been registered!')
+    {'username': 'obiwan', 'first_name': 'Obi-Wan', 'last_name': 'Kenobi',
+     'email': 'obi_wan@email.com', 'is_staff': False},
 
-u = User(username='skywalker', first_name='Luke', last_name='Skywalker', email='skywalker@email.com', is_staff=False)
-u.save()
-print('Luke Skywalker has been registered!')
+    {'username': 'skywalker', 'first_name': 'Luke', 'last_name': 'Skywalker',
+     'email': 'skywalker@email.com', 'is_staff': False},
 
-u = User(username='palpatine', first_name='Imperador', last_name='Palpatine', email='imperador_palpatine@email.com', is_staff=False)
-u.save()
-print('Imperador Palpatine has been registered!')
+    {'username': 'palpatine', 'first_name': 'Imperador', 'last_name': 'Palpatine',
+     'email': 'imperador_palpatine@email.com', 'is_staff': False},
 
-u = User(username='hansolo', first_name='Han', last_name='Solo', email='han_solo@email.com', is_staff=False)
-u.save()
-print('Darth Vader has been registered!')
+    {'username': 'hansolo', 'first_name': 'Han', 'last_name': 'Solo',
+     'email': 'han_solo@email.com', 'is_staff': False}
+]
+for i in list_names:
+    u = User(username=i['username'], first_name=i['first_name'], last_name=i['last_name'], 
+    email=i['email'], is_staff=i['is_staff'])
+    u.save()
+    print(f'{i["username"]} has been registered!')
