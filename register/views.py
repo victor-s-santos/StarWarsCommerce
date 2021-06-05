74261806796522f6b5fb19f83046be1c7a252146
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-05 15:45:56 -0300

    refactoring register views

--- register/views.py
+++ register/views.py
@@ -11,53 +11,66 @@ from decouple import config
 def register(request):
     """Realize the signup user"""
     if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            email_from = config('EMAIL_FROM')
-            form.save()
-            messages.success(request, "User has been registered successfully!")
-            username = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            auth_login(request, user)
-            body = render_to_string('register/email_signup.txt',
-                                    form.cleaned_data)
-            mail.send_mail('Sign up Confirmation',
-                            body,
-                            email_from,
-                            [email_from, form.cleaned_data['email']])
-            
-            return redirect('profile')
-        else:
-            messages.error(request, 'Email or username already registered.')
-            return render(request, 'register/register.html', {'form': form})
+        return create(request)
     else:
-        form = RegisterForm()
+        return new(request)
+
+def create(request):
+    form = RegisterForm(request.POST)
+    if form.is_valid():
+        email_from = config('EMAIL_FROM')
+        form.save()
+        messages.success(request, "User has been registered successfully!")
+        username = form.cleaned_data.get('email')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        auth_login(request, user)
+        body = render_to_string('register/email_signup.txt',
+                                form.cleaned_data)
+        mail.send_mail('Sign up Confirmation',
+                        body,
+                        email_from,
+                        [email_from, form.cleaned_data['email']])
+        
+        return redirect('profile')
+    else:
+        messages.error(request, 'Email or username already registered.')
+        return render(request, 'register/register.html', {'form': form})
+
+def new(request):
+    form = RegisterForm()
     return render(request, 'register/register.html', {'form': form})
 
 @login_required(login_url='/login/')
 def profile(request):
     """Get more information about just registered user"""
     if request.method == "POST":
-        form = ProfileForm(request.POST)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = request.user
-            profile.save()
-            messages.success(request, 'Thank you!')
-            return redirect('core:home')
-        else:
-            messages.error(request, 'Please review the information you are trying to submit.')
-            return render(request, 'register/infos.html', {'form': form})
+        return create_profile(request)
     else:
-        form = ProfileForm()
+        return new_profile(request)
+
+@login_required(login_url='/login/')
+def create_profile(request):
+    form = ProfileForm(request.POST)
+    if form.is_valid():
+        profile = form.save(commit=False)
+        profile.user = request.user
+        profile.save()
+        messages.success(request, 'Thank you!')
+        return redirect('core:home')
+    else:
+        messages.error(request, 'Please review the information you are trying to submit.')
+        return render(request, 'register/infos.html', {'form': form})
+    
+def new_profile(request):
+    form = ProfileForm()
     return render(request, 'register/infos.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request.POST)
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
-
         if user is not None:
             if user.is_active:
                 auth_login(request, user)
