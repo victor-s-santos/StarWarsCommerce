39fec62eddae733441ad90d657f19aca007cdad7
Victor Santos Silva <victor.s.silva@unesp.br>
2021-06-06 11:46:00 -0300

    deleting detail_view test

--- commerce/tests/test_commerce_status_code.py
+++ commerce/tests/test_commerce_status_code.py
@@ -1,4 +1,4 @@
-from django.urls import reverse
+from django.shortcuts import resolve_url as url
 import pytest
 
 list_reverse_names_authenticated = [
@@ -20,11 +20,11 @@ list_reverse_names_admin = [
 
 @pytest.mark.parametrize('path, status_code', list_reverse_names_authenticated)
 def test_render_views(client_with_user, path, status_code):
-    assert client_with_user.get(reverse(path)).status_code == status_code
+    assert client_with_user.get(url(path)).status_code == status_code
 
 @pytest.mark.parametrize('path, status_code', list_reverse_names_admin)
 def test_render_views_admin(admin_client, path, status_code):
-    assert admin_client.get(reverse(path)).status_code == status_code
+    assert admin_client.get(url(path)).status_code == status_code
 
 
 
